d6456099bebbff58d218b0f9b6a1b34e1097b7f8
Christian Hernandez <chrismatla68@gmail.com>
2021-03-21 23:38:11 -0600

    adding functions to check installations

--- load.sh
+++ load.sh
@@ -9,5 +9,27 @@ function replace_files {
 	sed -i -E 's/ZSH_THEME=\"(.*[a-zA-Z0-9])"/ZSH_THEME="golurk"/' ~/.zshrc
 }
 
+function install_vim_requirements {
+	# Vim Plug
+	sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+	# Vim Vundle
+	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+}
+
+function validate_installations {
+	# git
+	git --version 2>&1 >/dev/null
+	GIT_IS_AVAILABLE=$?
+	if [ $GIT_IS_AVAILABLE != 0 ]; then
+		# fedora
+		dnf install -y git
+	fi
+}
+
 move_files
 replace_files
+
+validate_installations
+install_vim_requirements
